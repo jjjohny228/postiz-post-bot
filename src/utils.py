a7fe4807fad9ae3b4bf3217f274a48e98adc5635
jjjohny228 <gggleb13@gmail.com>
2025-11-24 18:32:33 +0200

    Added fucntions to read csv file and post videos

---
+++ src/utils.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import requests
+import time
+from datetime import datetime, timedelta
+
+
+def post_tiktok_video(post_time: str, channel_id: str, description: str, video_url: str):
+    postiz_post_url = 'https://api.postiz.com/public/v1/posts'
+    data = {
+        "type": "schedule",
+        "tags": [],
+        "shortLink": "true",
+        "date": post_time,
+        "posts": [
+            {
+                "integration": {
+                    "id": channel_id
+                },
+                "value": [
+                    {
+                        "content": description,
+                        "image": [
+                            {
+                                "id": "string",
+                                "path": video_url
+                            }
+                        ]
+                    }
+                ],
+                "settings": {
+                    "__type": "tiktok",
+                    "privacy_level": "SELF_ONLY", # после авторизации заменить на PUBLIC_TO_EVERYONE, SELF_ONLY
+                    "duet": "false",
+                    "stitch": "false",
+                    "comment": "true",
+                    "autoAddMusic": "no",
+                    "brand_content_toggle": "false",
+                    "brand_organic_toggle": "false",
+                    "content_posting_method": "DIRECT_POST"
+                }
+            },
+        ],
+    }
+    headers = {
+        'Authorization': 'f2714674eee51183c7660db056a9f531674dfc3923b33520d6bd9694ff0f2e14',
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(postiz_post_url, json=data, headers=headers)
+    return response.status_code
+
+
+def test_unpack_csv_and_upload_video(file_csv: str):
+    """
+    This function unpacks csv file and upload videos to postiz
+    """
+
+
+def post_videos_from_csv(file_path):
+    """
+    Function reads csv file and posts videos.
+    Due to the postiz limit (30 api requests an hour), there is limitation to wait next hour
+    """
+    def wait_until_next_hour():
+        now = datetime.now()
+        next_hour = (now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1))
+        wait_seconds = (next_hour - now).total_seconds()
+        print(f"Ошибка, ждем {wait_seconds} секунд до начала нового часа...")
+        time.sleep(wait_seconds)
+
+    while True:
+        df = pd.read_csv(file_path)
+
+        if df.empty:
+            print("Файл пуст, работа завершена.")
+            break
+
+        row_index_to_drop = []
+
+        for idx, row in df.iterrows():
+            post_time = row['post_time']
+            channel_id = row['channel_id']
+            description = row['title'] if not np.isnan(row['title']) else ''
+            video_url = row['video_url']
+
+            # post video
+            result_status = post_tiktok_video(post_time, channel_id, description, video_url)
+            row_index_to_drop.append(idx)
+
+            if result_status == 429:
+                print('Postiz limit was exceeded. Need to wait next hour.')
+                wait_until_next_hour()
+            # After waiting, interrupt the current cycle to reread the file
+            break
+
+        # Delete successful lines
+        if row_index_to_drop:
+            df = df.drop(row_index_to_drop)
+            df.to_csv(file_path, index=False)
+        else:
+            # If there is an error, abort without deleting
+            break
+
+    return True
